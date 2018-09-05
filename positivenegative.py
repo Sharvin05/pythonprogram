4a442449dd31589f10a8c2e3fd981e3b4855201a
Sharvin05 <42992893+Sharvin05@users.noreply.github.com>
2018-09-05 12:23:37 +0530

    Update positivenegative.py

--- positivenegative.py
+++ positivenegative.py
@@ -1,7 +1,7 @@
 a=int(input())
-if(a>0):
-	print("Positive")
-elif(a<0):
-            print("Negative")
+if(a<0):
+	print("Negative")
+elif(a=0):
+	print("zero")
 else:
-	print("Zero")
+	print("positive")
